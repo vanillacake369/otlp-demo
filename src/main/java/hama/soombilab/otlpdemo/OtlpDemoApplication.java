af7966129f279cf59baffd9ae3957693a06e9e6d
limjihoon <lonelynight1026@hamagroups.io>
2025-04-25 14:47:53 +0900

    Initial commit

---
+++ src/main/java/hama/soombilab/otlpdemo/OtlpDemoApplication.java
@@ -0,0 +1,13 @@
+package hama.soombilab.otlpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtlpDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtlpDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hama/soombilab/otlpdemo/OtlpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package hama.soombilab.otlpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtlpDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
